ec5a0017b6e16b949b5e6f28c86971a3b42240a6
Babita00 <babitaasth07@gmail.com>
2025-01-28 16:55:05 +0545

    feat: update root reducer to use a more descriptive key for login reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,7 +1,7 @@
 // for combine reducer
-import { combineReducers } from 'redux';
-import { loginReducer } from './loginReducer';
+import { combineReducers } from "redux";
+import { loginReducer } from "./loginReducer";
 
 export default combineReducers({
-loginReducer
-});  // Compare this line from BillingApp/src/reducer/index.js: 
+  login: loginReducer,
+}); // Compare this line from BillingApp/src/reducer/index.js:
