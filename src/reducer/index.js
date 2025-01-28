5e7ae5312d290e1379ec928a21417df68319a1e7
Babita00 <babitaasth07@gmail.com>
2025-01-28 17:15:24 +0545

    feat: add register reducer to combine reducers

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,7 +1,9 @@
 // for combine reducer
 import { combineReducers } from "redux";
 import { loginReducer } from "./loginReducer";
+import { registerReducer } from "./registerReducer";
 
 export default combineReducers({
   login: loginReducer,
+  register: registerReducer,
 }); // Compare this line from BillingApp/src/reducer/index.js:
