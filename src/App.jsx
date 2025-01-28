40819f741ce8f219030a5d46b502e61f582f74b9
Babita00 <babitaasth07@gmail.com>
2025-01-28 15:09:14 +0545

    add main application structure with routing and page components

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AboutPage from "./components/AboutPage/AboutPage";
+import FeaturePage from "./components/FeaturePage/FeaturePage";
+import PricingPage from "./components/PricingPage/PricingPage";
+import NewsPage from "./components/NewsPage/NewsPage";
+import ContactPage from "./components/ContactPage/ContactPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import HomePage from "./components/HomePage/HomePage";
+import Path from "./Path";
+import Main from "./Main/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Path />,
+    children: [
+      {
+        path: "/home",
+        element: <HomePage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "feature",
+        element: <FeaturePage />,
+      },
+      {
+        path: "pricing",
+        element: <PricingPage />,
+      },
+      {
+        path: "news",
+        element: <NewsPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "main",
+        element: <Main />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
