02223ad9b462366d0e2826f2562352ba401ee08b
Babita00 <babitaasth07@gmail.com>
2025-01-28 16:54:18 +0545

    feat: add root reducer to combine login reducer

---
+++ src/reducer/index.js
@@ -0,0 +1,7 @@
+// for combine reducer
+import { combineReducers } from 'redux';
+import { loginReducer } from './loginReducer';
+
+export default combineReducers({
+loginReducer
+});  // Compare this line from BillingApp/src/reducer/index.js: 
