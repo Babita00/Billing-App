48628798b12d51621d99854105db24aacfdbfc7f
Babita00 <babitaasth07@gmail.com>
2025-01-28 15:24:14 +0545

    refactor HomePage to use RegisterDialogue component instead of RegisterDialog

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { RegisterDialog } from "../../pages/RegisterDialog";
+// import { RegisterDialog } from "../../pages/RegisterDialog";
+import { RegisterDialogue } from "../../pages/RegisterDialogue";
 const HomePage = () => {
-const [showDialog, setShowDialog] = useState(false);
+  const [showDialog, setShowDialog] = useState(false);
 
   const onRegisterClick = () => {
     setShowDialog(!showDialog);
-  }
+  };
 
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-r from-teal-50 to-blue-50 p-6">
@@ -19,10 +20,16 @@ const [showDialog, setShowDialog] = useState(false);
             You may start selling in a matter of minutes and easy to use.
             Appropriate for all devices.
           </p>
-          <button className="bg-blue-600 text-white py-3 px-6 rounded-lg text-lg font-medium hover:bg-blue-700" onClick={onRegisterClick}>
+          <button
+            className="bg-blue-600 text-white py-3 px-6 rounded-lg text-lg font-medium hover:bg-blue-700"
+            onClick={onRegisterClick}
+          >
             Register
           </button>
-          {showDialog && <RegisterDialog showD={showDialog} onHide={setShowDialog}/>}
+          {/* {showDialog && <RegisterDialog showD={showDialog} onHide={setShowDialog}/>} */}
+          {showDialog && (
+            <RegisterDialogue showD={showDialog} onHide={setShowDialog} />
+          )}
         </div>
         <div className="mt-8 md:mt-0 bg-gradient-to-r from-purple-100 to-pink-100 rounded-full w-80 h-80 md:w-112 md:h-112 flex items-center justify-center">
           <img
