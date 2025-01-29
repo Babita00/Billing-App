8ee0a34841ccf9e4d2510fb1982a645b59668da8
Babita00 <babitaasth07@gmail.com>
2025-01-29 16:53:07 +0545

    feat: integrate redux-thunk middleware for enhanced asynchronous action handling

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootreducer from "../reducer";
-const store = createStore(rootreducer);
+import {thunk} from "redux-thunk";
+const store = createStore(rootreducer,applyMiddleware(thunk));
 export default store;
