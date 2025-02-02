c1d79dfa2fd5d57aaa8e820b5551992c4ff85c98
Babita00 <babitaasth07@gmail.com>
2025-02-02 14:59:20 +0545

    feat: add posReducer to combineReducers for product state management

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,8 +2,9 @@
 import { combineReducers } from "redux";
 import { loginReducer } from "./loginReducer";
 import { registerReducer } from "./registerReducer";
-
+import posReducer from "./posReducer";
 export default combineReducers({
   login: loginReducer,
   register: registerReducer,
+  products: posReducer,
 }); // Compare this line from BillingApp/src/reducer/index.js:
