e9294aa66e0ba45e9c483da779e488f35246c333
Babita00 <babitaasth07@gmail.com>
2025-02-02 15:01:46 +0545

    refactor: rename posReducer to productItemsReducer in combineReducers for clarity

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,9 +2,9 @@
 import { combineReducers } from "redux";
 import { loginReducer } from "./loginReducer";
 import { registerReducer } from "./registerReducer";
-import posReducer from "./posReducer";
+import productItemsReducer from "./posReducer";
 export default combineReducers({
   login: loginReducer,
   register: registerReducer,
-  products: posReducer,
+  products: productItemsReducer,
 }); // Compare this line from BillingApp/src/reducer/index.js:
