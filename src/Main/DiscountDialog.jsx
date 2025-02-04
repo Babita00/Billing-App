018afeba0a36a4238f70eac220ea7a895f4d6e2d
Babita00 <babitaasth07@gmail.com>
2025-02-04 15:25:56 +0545

    feat: add DiscountModal component for applying discounts

---
+++ src/Main/DiscountDialog.jsx
@@ -0,0 +1,69 @@
+import { Button, Space, Modal, Form, Radio, InputNumber } from "antd";
+
+const DiscountModal = (props) => {
+  const { isDiscountModalVisible, handleDiscountSubmit, discountForm } = props;
+  return (
+    <Modal
+      title="Apply Discount"
+      open={isDiscountModalVisible}
+      onCancel={() => setIsDiscountModalVisible(false)}
+      footer={null}
+    >
+      <Form
+        form={discountForm}
+        onFinish={handleDiscountSubmit}
+        initialValues={{
+          discountType: "fixed",
+          discountAmount: 0,
+        }}
+        layout="vertical"
+      >
+        <Form.Item name="discountType" label="Discount Type">
+          <Radio.Group>
+            <Radio value="fixed">Fixed Amount ($)</Radio>
+            <Radio value="percentage">Percentage (%)</Radio>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item
+          name="discountAmount"
+          label="Discount Amount"
+          rules={[
+            { required: true, message: "Please enter discount amount" },
+            { type: "number", min: 0, message: "Amount must be positive" },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (
+                  getFieldValue("discountType") === "percentage" &&
+                  value > 100
+                ) {
+                  return Promise.reject("Percentage cannot exceed 100%");
+                }
+                return Promise.resolve();
+              },
+            }),
+          ]}
+        >
+          <InputNumber
+            style={{ width: "100%" }}
+            placeholder="Enter discount amount"
+            precision={2}
+          />
+        </Form.Item>
+
+        <Form.Item className="flex justify-end mb-0">
+          <Space>
+            <Button onClick={() => setIsDiscountModalVisible(false)}>
+              Cancel
+            </Button>
+            <Button type="primary" htmlType="submit">
+              Apply Discount
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default DiscountModal;
