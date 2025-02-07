213f9517df15c87677d662035498a1c221c53533
Babita00 <babitaasth07@gmail.com>
2025-02-07 15:53:38 +0545

    feat: implement saveOrder action with loader handling for order submission

--- src/actions/posActions.jsx
+++ src/actions/posActions.jsx
@@ -1,16 +1,16 @@
-import products from "../config/products.json"
+import products from "../config/products.json";
 
 import { actionType } from "../constants/constants";
+import { showLoader, hideLoader } from "./loaderAcrion";
+import { SAVE_ORDER } from "../constants/constants";
 
 export const posAction = () => {
   return async (dispatch) => {
     dispatch({
-
-        //api call
+      //api call
       type: actionType.GET_PRODUCT_ITEMS,
       payload: products,
     });
-   
   };
 };
 
@@ -24,3 +24,25 @@ export const posAction = () => {
 //     });
 //   };
 // };
+
+export const saveOrder = (request) => {
+  return async (dispatch) => {
+    dispatch(showLoader());
+
+    const response = await fetch(SAVE_ORDER, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    });
+    const result = await response.json();
+    // const result = {status: true, message: 'Order Save Success'}
+    dispatch(hideLoader());
+
+    dispatch({
+      type: actionType.SAVE_ORDER,
+      payload: result,
+    });
+  };
+};
