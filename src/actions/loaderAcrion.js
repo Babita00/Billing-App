43cd44fe392a3a708037091dcf9b2e816eda107f
Babita00 <babitaasth07@gmail.com>
2025-02-07 15:53:31 +0545

    feat: add loader actions for showing and hiding loader state

---
+++ src/actions/loaderAcrion.js
@@ -0,0 +1,8 @@
+import { loaderType } from "../constants/constants";
+
+export const showLoader = () => {
+  return { type: loaderType.SHOW };
+};
+export const hideLoader = () => {
+  return { type: loaderType.SHOW };
+};
