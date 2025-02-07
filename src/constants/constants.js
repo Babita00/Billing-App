7f949c6b1d8e31b3d7e68c34762830eaa0fd29b5
Babita00 <babitaasth07@gmail.com>
2025-02-07 15:53:45 +0545

    feat: add loaderType constants and SAVE_ORDER endpoint to constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,9 +2,15 @@ export const actionType = {
   LOGIN: "LOGIN",
   REGISTER: "REGISTER",
   GET_PRODUCT_ITEMS: "GET_PRODUCT_ITEMS",
-
 };
 
-
-export const DEFAULT_PRODUCT_IMAGE = "https://waiwai.com.np/wp-content/uploads/2023/05/Wai-Wai-Chicken-1-1.png";
+export const DEFAULT_PRODUCT_IMAGE =
+  "https://waiwai.com.np/wp-content/uploads/2023/05/Wai-Wai-Chicken-1-1.png";
 export const STORE_NAME = "NEPKODER STORE";
+
+export const loaderType = {
+  SHOW: "SHOW",
+  HIDE: "HIDE",
+};
+
+export const SAVE_ORDER = "https://emisewa.com/public/save-order";
